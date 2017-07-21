c4314223b16c7c9d9dc1203cf29dc14e3c5f0cfe
r0tiart <antley.chu@gmail.com>
2017-07-21 10:07:02 -0400

    edited friendship and user association, now users have many friends via friendship join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	has_many :user_books
 	has_many :books, through: :user_books
-	has_many :friendships, :foreign_key => "user_id", :class_name => "Friendships"
+	has_many :friendships, :foreign_key => "user_id", :class_name => "Friendship"
 	has_many :friends, :through => :friendships
 
 	has_secure_password
