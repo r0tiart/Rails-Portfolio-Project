5c503ba121ef2a4958294ebbd99e43b605e1c69d
r0tiart <antley.chu@gmail.com>
2017-07-21 09:51:15 -0400

    added an auto incremented friend_id starting with 1 in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ApplicationRecord
 	has_many :friendships, :foreign_key => "user_id", :class_name => "Friendships"
 	has_many :friends, :through => :friendships
 
+	has_secure_password
 
+	before_create :set_friend_id
+	def set_friend_id
+  		last_friend_id = User.maximum(:friend_id)
+ 		if !!last_friend_id
+  			self.friend_id = last_friend_id + 1
+  		else
+  			self.friend_id = 1
+  		end
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721120902) do
+ActiveRecord::Schema.define(version: 20170721125905) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -27,8 +27,30 @@ ActiveRecord::Schema.define(version: 20170721120902) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "friendships", force: :cascade do |t|
+    t.string "user_id"
+    t.string "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "genres", force: :cascade do |t|
     t.string "title"
+  end
+
+  create_table "user_books", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
