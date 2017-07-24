68fab5c327d7b5ad5995011ae46189d0e8fff4d5
r0tiart <antley.chu@gmail.com>
2017-07-24 19:30:48 -0400

    added some tests for user signup

--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -4,9 +4,16 @@ describe 'Feature Test: User', :type => :feature do
 
 	it 'successfully signs up as non-admin' do
 	    visit '/'
-	    expect(current_path).to eq('/users/new')
-	    user_signup
+	    click_link('Sign Up')
+
+	   	expect(current_path).to eq('/users/new')
+
+	    fill_in("user[username]", :with => "Amy Poehler")
+	    fill_in("user[password]", :with => "password")
+	    click_button('Create User')
+
 	    expect(current_path).to eq('/users/1')
+	    expect(page).to have_content("Amy Poehler")
 	end
   
 end
