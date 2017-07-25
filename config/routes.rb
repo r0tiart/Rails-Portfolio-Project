8630b5c54fefac37d6eafabc30bde23636d68c68
r0tiart <antley.chu@gmail.com>
2017-07-24 20:23:41 -0400

    added links to new form and edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "application#welcome"
-  resources :user
+  resources :users, only: [:new, :create, :index, :show, :update, :edit]
 end
