aafab4bc47d15fa157e5167a5e483b01c75a491f
r0tiart <antley.chu@gmail.com>
2017-07-26 16:38:45 -0400

    added a friend show page link, with tests for user/1/friends

---
+++ spec/features/friends_features_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe 'Feature Test: Friend', :type => :feature do 
+
+	before do
+		User.destroy_all
+		Author.destroy_all
+		Book.destroy_all
+		Genre.destroy_all
+
+		@user = User.create(
+	      username: "user",
+	      password: "password",
+	    )
+
+	    @user2 = User.create(
+	      username: "user2",
+	      password: "password"
+	    )
+
+	    @book = Book.create(title: "book1")
+	    @author = Author.create(name: "author1")
+	    @genre = Genre.create(title: "genre1")
+	    @book.author = @author
+	    @book.genre = @genre
+		@book.save
+
+		@user.user_books.create(book_id: @book.id)
+		@user.friend_request=(@user2)
+
+
+	   	visit "/"
+	    click_link('Log In')
+
+	    fill_in("user[username]", :with => "user")
+	    fill_in("user[password]", :with => "password")
+
+	    click_button('Sign In')
+	end
+
+	it 'has a friend list - of all accepted friends' do
+		@user2.accept_friend_request(@user)
+		visit '/users/1/friends'
+		expect(page).to have_content(@user.username)
+		visit '/users/2/friends'
+		expect(page).to have_content(@user2.username)
+	end
+
+	it 'has a pending friend list' do 
+		@user.friend_request=(@user2)
+	end
+end
