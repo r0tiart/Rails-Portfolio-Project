d9d7e762c12b30b8dbe7a3dc9f4bc5f0e554d8ca
r0tiart <antley.chu@gmail.com>
2017-07-27 14:26:52 -0400

    added edit page to work with bookmark link

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,5 +10,4 @@ class BooksController < ApplicationController
 	def show 
 		@book = Book.find_by(id: params[:id])
 	end
-
 end
--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -3,4 +3,9 @@ class UserBooksController < ApplicationController
 		current_user.user_books.create(book_id: params[:id])
 		redirect_to user_path(current_user)
 	end
+
+	def edit 
+		@book = UserBook.find_by(params[:id])
+	end
+
 end
