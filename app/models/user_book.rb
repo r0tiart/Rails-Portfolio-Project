58f0ffa282dd5922b9c154ff3390278d8a308965
r0tiart <antley.chu@gmail.com>
2017-07-27 16:07:06 -0400

    eddit validation for page number

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -18,7 +18,10 @@ class UserBook < ApplicationRecord
 	private
 
 	def page_must_be_number_or_completed
-		if self.page.to_i == 0 && self.page.downcase != "completed"
+		string = page.scan(/\d+|\D+/)
+		array = string.collect{|element| element.to_i}
+
+		if array.include?(0) && self.page.downcase != "completed"
 			 errors.add(:page, "must be a page number or 'completed' ")
 		end
 	end
